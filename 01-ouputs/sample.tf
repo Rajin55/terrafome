b9e79a67a28e121d2d17eef558ad24d0db33c871
saisindhu96 <saisindhuraju@gmail.com>
2022-06-03 23:52:22 +0530

    add

--- 01-ouputs/sample.tf
+++ 01-ouputs/sample.tf
@@ -1,3 +1,45 @@
 output "sample" {
-  default= "Hello Worls"
+  //value = "Hello World
+  value = var.sample
+}
+
+
+output "sample1" {
+  value = "${var.sample} Welcom you"
+}
+
+variable "sample" {
+  default = "Hello WOrld"
+}
+
+## vaiable sample be accessed as var.sample or ${var.sample}
+## ${var.sample} is used within quotes
+## var.sample is used without qoutes
+
+# Nuber data type
+
+variable "sample2" {
+  default = 100
+}
+output "sample2" {
+  value = var.sample2
+}
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+output "sample3" {
+  value = var.sample3
+}
+
+variable "sample4" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+output "sample4" {
+  value = var.sample4
 }
