0269f9be71e6d388a50ae17cee0b1bf851696f0c
saisindhu96 <saisindhuraju@gmail.com>
2022-06-07 12:41:10 +0530

    add

--- examples/ec2instance-with-sg/ec2.tf
+++ examples/ec2instance-with-sg/ec2.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "sample" {
   ami           = "ami-0bb6af715826253bf"
   instance_type = "t2.micro"
+  tags = {
+    Name = "Test"
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
 }
