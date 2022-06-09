07d5bc4b7e0ee16853d87057c2b7dcd7fbc8ce8c
saisindhu96 <saisindhuraju@gmail.com>
2022-06-09 18:38:54 +0530

    add

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -1,7 +1,16 @@
-resource "aws_instance" "sample" {
+resource "aws_instance" "cheap_worker" {
+  count         =length(var.COMPONENTS)
   ami           = "ami-0bb6af715826253bf"
   instance_type = "t2.micro"
   tags = {
-    Name = "Test"
+    Name        = element(var.COMPONENTS, count.index )
   }
 }
+
+variable "COMPONENTS" {}
+
+//resource "time_sleep" "wait" {
+ // depends_on = [aws_spot_instance_request.cheap_worker]
+ // create_duration = "120s"
+//}
+//resource "" "" {}
