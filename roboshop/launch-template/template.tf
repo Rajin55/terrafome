8a65e61ee0f5299cbb14c07399a18c6f18a3e85f
saisindhu96 <saisindhuraju@gmail.com>
2022-06-09 18:08:13 +0530

    add

---
+++ roboshop/launch-template/template.tf
@@ -0,0 +1,9 @@
+esource "aws_launch_template" "foo" {
+  name     = "template-for-roboshop"
+  image_id = "ami-0bb6af715826253bf"
+  instance_market_options {
+    market_type = "spot"
+  }
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0efa67d7efc0a9726"]
+}
